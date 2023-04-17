ea721d515c0d891921906407fcd73c71f7d95612
JakubZubek <jakub.zubek.1998@gmail.com>
2023-04-17 18:40:12 +0200

    editing strings and creating dataframe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-
+import pandas as pd
+import re
+import clean
 list_of_league_urls = [
     'https://www.transfermarkt.pl/serie-a/startseite/wettbewerb/IT1',
     'https://www.transfermarkt.pl/ligue-1/startseite/wettbewerb/FR1',
@@ -11,6 +13,10 @@ list_of_league_urls = [
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
 
+
+all_footballers = []
+
+
 def parse_leauge():
     for league_url in list_of_league_urls:
         league_page = requests.get(league_url, headers=headers)
@@ -18,6 +24,10 @@ def parse_leauge():
         league_name = (leauge_soup.title.string)
         league_name = league_name[:-22]  # delete 'yy/yy TransferMarkt'
         parse_club(leauge_soup)
+        
+    return (all_footballers)
+
+
 
 def parse_club(league):
     for link in league.find_all('td', class_="hauptlink no-border-links"):
@@ -31,45 +41,40 @@ def parse_club(league):
 
 def parse_footballer(club):
         for footballer in (club.find_all('td', class_="hauptlink")):
-            if "profil" in footballer.a.get("href"):
-                
-                footballer_url = footballer.a.get("href")
-                footballer_url = "https://www.transfermarkt.com" + footballer_url
-                footballer_page = requests.get(footballer_url, headers=headers)
-                footballer_soup = BeautifulSoup(footballer_page.content, 'html.parser')
-                footballer_name = footballer_soup.find('h1', class_="data-header__headline-wrapper").text
-                footballer_positions = [data.text for data in footballer_soup.find_all('dd', class_="detail-position__position")]
-                value = footballer_soup.find('div', class_="tm-player-market-value-development__current-value").text
-                footballer_details = [data.text for data in footballer_soup.find_all('li', class_="data-header__label")]
-                footballer_info_pack = [clearance(footballer_name), footballer_positions, list_clearance(footballer_details), value]
-                footballer_info_pack = [list_clearance(footballer_details)]
-                footballer_info_pack.append(clearance(footballer_name))
-                footballer_info_pack.append(value)
-                footballer_info_pack.append(footballer_positions)
-                result = [item for sublist in footballer_info_pack for item in sublist]
-                print (footballer_info_pack)
+            try:
+                if "profil" in footballer.a.get("href"):
+                    footballer_url = footballer.a.get("href")
+                    footballer_url = "https://www.transfermarkt.com" + footballer_url
+                    footballer_page = requests.get(footballer_url, headers=headers)
+                    footballer_soup = BeautifulSoup(footballer_page.content, 'html.parser')
+                    club_name = footballer_soup.find('span', class_="data-header__club").text
+                    league_name = footballer_soup.find('a', class_="data-header__league-link").text
+                    footballer_name = footballer_soup.find('h1', class_="data-header__headline-wrapper").text
+                    footballer_name = re.sub(r'[^a-zA-Z ]+', '',footballer_name)
+                    value = footballer_soup.find('div', class_="tm-player-market-value-development__current-value").text
+                    footballer_details = [data.text for data in footballer_soup.find_all('li', class_="data-header__label")]
+                    footballer_info_pack = clean.list_clearance(footballer_details)
+                    
+                    footballer_name = clean.clearance(footballer_name)
+                    footballer_info_pack["Name"] = footballer_name
+
+                    value = clean.value_count(value)
+                    footballer_info_pack["Value in million"] = value
+
+                    club_name = clean.clearance(club_name)
+                    footballer_info_pack["Club"] = club_name
+
+                    league_name = clean.clearance(league_name)
+                    footballer_info_pack["Leauge"] = league_name
+
+                    all_footballers.append(footballer_info_pack)
+            except:
+                pass
+            break
+
+def create_df_and_save_to_csv(data):
+    df = pd.DataFrame(data)
+    df.to_csv("Players.csv", index=False)
 
 
-
-
-
-
-
-                break
-
-def list_clearance(data_list):
-    clean_list = []
-    for data in data_list:
-        if "Agent" not in data:
-            clean_list.append(clearance(data))
-    return clean_list
-
-
-def clearance(data):
-    result = data.replace("\n", "")
-    result = result.replace("  ", "")
-    return result
-
-
-
-parse_leauge()
+create_df_and_save_to_csv(parse_leauge())
